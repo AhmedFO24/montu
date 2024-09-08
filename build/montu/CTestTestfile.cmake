5d33aa6f5d93b11e5fb9af3b6cb6892b2968f827
AhmedFO24 <ahmedelsharkawy35@gmail.com>
2024-09-08 13:56:03 +0300

    only montu package

--- build/montu/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/montu/catkin_ws/src/montu
-# Build directory: /home/montu/catkin_ws/build/montu
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
