8e4af5daacd18322b2e4e42989a1b956bb46ce9d
AhmedFO24 <ahmedelsharkawy35@gmail.com>
2024-09-26 16:48:04 +0300

    915

--- src/scripts/drive_UGV_dual_serial_read_power_3.py
+++ src/scripts/drive_UGV_dual_serial_read_power_3.py
@@ -187,7 +187,7 @@ def callback(msg):
         status_msg = "Stopped"
     
     # Converting Readings from 0 to 1000
-    driver_speed[0] = round(((rpm_right + MAX_RPM) * 925/MAX_RPM) - 925)
+    driver_speed[0] = round(((rpm_right + MAX_RPM) * 915/MAX_RPM) - 915)
     driver_speed[1] = round(((rpm_left + MAX_RPM) * 1000/MAX_RPM) - 1000)
     
     # This for preventing Skid Steering
