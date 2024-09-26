358a3b0abe8f89c6a06abb794b5a523016a4ce98
AhmedFO24 <ahmedelsharkawy35@gmail.com>
2024-09-26 16:23:49 +0300

    rpm driver 500

--- src/scripts/drive_UGV_dual_serial_read_power_3.py
+++ src/scripts/drive_UGV_dual_serial_read_power_3.py
@@ -187,7 +187,7 @@ def callback(msg):
         status_msg = "Stopped"
     
     # Converting Readings from 0 to 1000
-    driver_speed[0] = round(((rpm_right + MAX_RPM) * 800/MAX_RPM) - 800)
+    driver_speed[0] = round(((rpm_right + MAX_RPM) * 500/MAX_RPM) - 500)
     driver_speed[1] = round(((rpm_left + MAX_RPM) * 1000/MAX_RPM) - 1000)
     
     # This for preventing Skid Steering
