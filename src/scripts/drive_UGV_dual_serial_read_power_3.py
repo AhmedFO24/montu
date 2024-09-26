541eaa981df7c5108ff3a19d2a1aefcfcd2712b8
AhmedFO24 <ahmedelsharkawy35@gmail.com>
2024-09-26 16:19:04 +0300

    rpm 2

--- src/scripts/drive_UGV_dual_serial_read_power_3.py
+++ src/scripts/drive_UGV_dual_serial_read_power_3.py
@@ -25,7 +25,7 @@ import socket
 import os 
 from datetime import datetime
 
-MAX_RPM_RIGHT = 0  # maximum wheel speed (rpm)
+MAX_RPM_RIGHT = 2  # maximum wheel speed (rpm)
 MAX_RPM_LEFT = 116
 
 driver_speed = [0]*2 # [0, 0]
